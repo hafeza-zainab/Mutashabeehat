77769a15c3cd689d923eecf8f7005bdd938e1d2f
hafeza-zainab <hafeza.zainab@gmail.com>
2026-06-08 15:56:42 +0530

    Delete backend directory

--- backend/modules/similarity/similarity.routes.js
+++
@@ -1,8 +0,0 @@
-//C:\quran-similarity-app\backend\modules\similarity\similarity.routes.js
-const express    = require("express");
-const router     = express.Router();
-const controller = require("./similarity.controller");
-
-router.get("/", controller.getSimilarities);
-
-module.exports = router;
