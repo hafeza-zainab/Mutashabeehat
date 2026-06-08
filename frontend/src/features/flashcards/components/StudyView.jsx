57490a4c2c2d03edd9ee66682949214ab803043e
hafeza-zainab <hafeza.zainab@gmail.com>
2026-06-08 15:57:00 +0530

    Delete frontend directory

--- frontend/src/features/flashcards/components/StudyView.jsx
+++
@@ -1,8 +0,0 @@
-//C:\quran-similarity-app\frontend\src\features\flashcards\components\StudyView.jsx
-import React from 'react';
-
-export default function StudyView({ category }) {
-  return (
-    <div className="study-view-container" dangerouslySetInnerHTML={{ __html: category.study }} />
-  );
-}
